a7e705ec2ffb75e6c147ffbb17c02c8baf17f444
michaelmclennan <michael.james.mclennan@gmail.com>
2020-10-11 00:29:55 +0000

    added 3d

--- Pathfinder.cpp
+++ Pathfinder.cpp
@@ -43,21 +43,25 @@
 	*/
 	bool Pathfinder::importMaze(string file_name)
 	{
-		cout << "importMaze from "<<file_name<<endl;
-		ifstream file (file_name.c_str());
-		if (file.is_open()) {
-			string line;
-			int row_count = 0;
-			for(int row = 0; row < ROW_SIZE; row++) {
+	cout << "importMaze from " << file_name << endl;
+	ifstream file(file_name);
+
+	if (file.is_open()) {
+		string line;
+		int depth_count = 0;
+		int row_count = 0;
+		for (int depth = 0; depth < DEPTH_SIZE; depth++) {
+			for (int row = 0; row < ROW_SIZE; row++) {
 				getline(file, line);
 				stringstream ss(line);
-				for(int col = 0; col < COL_SIZE; col++) {
+
+				for (int col = 0; col < COL_SIZE; col++) {
 					int value;
 					ss >> value;
-					// cout << "["<<row<<"]["<<col<<"]="<<value<<endl;
-					maze_grid[row][col] = value;
+					maze_grid[col][row][depth] = value;
 				}
 			}
+			getline(file, line);
 		}
 	    return(true);
 	}
